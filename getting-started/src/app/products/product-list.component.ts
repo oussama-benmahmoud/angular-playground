27d2b27499bb6aa05652a4e540e5812e0f97ddf1
Oussama BEN MAHMOUD <bm.oussema@gmail.com>
2020-10-15 20:31:44 +0200

    [TRAIN-9] added routing and navigation

--- getting-started/src/app/products/product-list.component.ts
+++ getting-started/src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@ import {IProduct} from './product';
 import {ProductService} from './product.service';
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
@@ -28,8 +27,7 @@ export class ProductListComponent implements OnInit {
 
   products: IProduct[] = [];
 
-  constructor(private productService: ProductService) {
-  }
+  constructor(private productService: ProductService) {}
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
